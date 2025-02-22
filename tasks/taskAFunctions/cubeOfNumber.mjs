71e78d567dad2faef60822501c8e8f5cf0cc7450
TheSteeNos <davidsteens2001@gmail.com>
2025-02-22 18:58:23 +0100

    Implemented tests, no changes necessary

--- tasks/taskAFunctions/cubeOfNumber.mjs
+++ tasks/taskAFunctions/cubeOfNumber.mjs
@@ -7,3 +7,12 @@ function cubeFinder(num) {
     num = +num
     return num*num*num
 }
+
+tests.isEqual(cubeFinder(4), 64, 'Expects cube of 4 which is 64');
+tests.isEqual(cubeFinder(0), 0, 'Expects cube of 0 which is 0');
+tests.isEqual(cubeFinder(-4), -64, 'Expects cube of -4 which is -64');
+tests.isEqual(cubeFinder('4'), 64, 'Expects to modify strings to integers, expects cube of 4 which is 64');
+tests.isNotANumber(cubeFinder('hello world'), 'Expects NaN if input is not a number');
+tests.isNotANumber(cubeFinder(NaN), 'Expects NaN if input is not a number');
+tests.isEqual(cubeFinder(Infinity), Infinity, 'Expects Infinity');
+tests.isEqual(cubeFinder(-Infinity), -Infinity, 'Expects -Infinity');
--- tasks/taskAFunctions/squareOfNumber.mjs
+++ tasks/taskAFunctions/squareOfNumber.mjs
@@ -10,7 +10,7 @@ function squareFinder(num) {
 
 tests.isEqual(squareFinder(4), 16, 'Expects square of 4 which is 16');
 tests.isEqual(squareFinder(0), 0, 'Expects square of 0 which is 0');
-tests.isEqual(squareFinder(4), 16, 'Expects square of -4 which is 16');
+tests.isEqual(squareFinder(-4), 16, 'Expects square of -4 which is 16');
 tests.isEqual(squareFinder('4'), 16, 'Expects to modify strings to integers, expects square of 4 which is 16');
 tests.isNotANumber(squareFinder('hello world'), 'Expects NaN if input is not a number');
 tests.isNotANumber(squareFinder(NaN), 'Expects NaN if input is not a number');
