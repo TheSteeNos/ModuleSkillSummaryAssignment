b7789614525419e41c99d69eb1f79731c8c7ba18
TheSteeNos <davidsteens2001@gmail.com>
2025-02-22 19:10:09 +0100

    Made function for area of Circle using Radius

---
+++ tasks/taskAFunctions/areaOfCircleUsingRadius.mjs
@@ -0,0 +1,9 @@
+import test from "../../tools/test.mjs";
+const tests = test("Area of Circle using Radius Test");
+
+
+
+function areaOfCircleUsingRadius(r) {
+    let pi = 3.1415926;
+    return r * r * pi;
+}
