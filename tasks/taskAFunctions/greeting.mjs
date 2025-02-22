a7eab7850ba4366d8a6367b91e29ed7947a3b56c
TheSteeNos <davidsteens2001@gmail.com>
2025-02-22 23:12:11 +0100

    Implemented tests, and changed function accordingly

--- tasks/taskAFunctions/greeting.mjs
+++ tasks/taskAFunctions/greeting.mjs
@@ -4,5 +4,21 @@ const tests = test("Greeting a Name Test");
 
 
 function greeting(name) {
+    if (typeof name !== "string" || /[^a-zA-Z\s]/.test(name)) return null;
+    name = name.trim();
+    if (name === "") return "No name entered";
+    let letters = name.split(/\s+/);
+    for (let i = 0; i < letters.length; i++) {
+        letters[i] = letters[i].charAt(0).toUpperCase() + letters[i].slice(1).toLowerCase();
+    }
+    name = letters.join(' ');
+
     return "Hello " + name + "! How are you?"
 }
+
+tests.isEqual(greeting(4), null, "Expects null as input isn't a string");
+tests.isEqual(greeting('     Donald  '), 'Hello Donald! How are you?', 'Expects removal of spaces in front and behind');
+tests.isEqual(greeting('donALd'), 'Hello Donald! How are you?', 'Expects to capitalize first letters of the name');
+tests.isEqual(greeting('donald duck'), 'Hello Donald Duck! How are you?', 'Expects to properly handle double names');
+tests.isEqual(greeting('  '), 'No name entered', 'Expects "No name entered"');
+tests.isEqual(greeting('donald@gmail.com'), null, 'Expects null as input has special characters');
