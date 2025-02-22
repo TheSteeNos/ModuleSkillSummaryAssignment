91fb55814f3fc0a70c52ca4db8345a7240b5a3b6
TheSteeNos <davidsteens2001@gmail.com>
2025-02-22 17:19:18 +0100

    Integrated tests, and changed code to give all greens

--- tasks/taskAFunctions/squareRoot.mjs
+++ tasks/taskAFunctions/squareRoot.mjs
@@ -1,9 +1,15 @@
+import test from "../../tools/test.mjs";
+const tests = test("Multiplication test");
+
 
 //Calculating square root using the Babylonian formula
 
 function squareRoot(num){
-    if (num < 0) return NaN;
+    num = +num
+    if (num < 0 || isNaN(num)) return NaN;
     if (num === 0 || num === 1) return num;
+    if (num === Infinity) return Infinity;
+    if (num === -Infinity) return NaN;
 
     let x = num;
     let y = (x + num / x) / 2;
@@ -15,3 +21,12 @@ function squareRoot(num){
 
     return y;
 }
+
+tests.isEqual(squareRoot(16), 4, 'Expects square root of 16 which is 4');
+tests.isEqual(squareRoot(0), 0, 'Expects 0 if input is 0');
+tests.isEqual(squareRoot(-16), NaN, 'Expects NaN, as there is no square root of a negative number');
+tests.isEqual(squareRoot('16'), 4, 'Expects to modify strings to integers, then multiply, expects 540');
+tests.isNotANumber(squareRoot('hello world'), 'Expects NaN if input is not a number');
+tests.isNotANumber(squareRoot(NaN, 3, 5, 9), 'Expects NaN if input is NaN');
+tests.isEqual(squareRoot(Infinity), Infinity, 'Expects Infinity');
+tests.isEqual(squareRoot(-Infinity), NaN, 'Expects NaN, as there is no square root of a negative number');
---
+++ tools/test.mjs
@@ -0,0 +1,51 @@
+
+function test(description) {
+
+    console.log(description);
+
+    return {
+        isEqual,
+        isNotANumber,
+        dosNotThrowError
+    }
+}
+
+function dosNotThrowError(testFunction, description) {
+
+    try {
+        testFunction();
+        console.log(`🟢 ${description}`);
+    } catch (error) {
+        console.log(error);
+        console.log(`🔴 ${description}`)
+    }
+
+}
+
+
+function isInRange(received, expectedLowerBound, expectedUpperBound, description) {
+
+    if (received >= expectedLowerBound && received <= expectedUpperBound) {
+        console.log(`🟢 ${description}`);
+    } else {
+        console.log(`🔴 ${description}. Expected ${expected}, received ${received}`);
+    }
+}
+
+function isNotANumber(received, description) {
+    if (isNaN(received)) {
+        console.log(`🟢 ${description}`);
+    } else {
+        console.log(`🔴 ${description}. received ${received}`);
+    }
+}
+
+function isEqual(received, expected, description) {
+    if (received === expected || (Number.isNaN(received) && Number.isNaN(expected))) {
+        console.log(`🟢 ${description}`);
+    } else {
+        console.log(`🔴 ${description}. Expected ${expected}, received ${received}`);
+    }
+}
+
+export default test
