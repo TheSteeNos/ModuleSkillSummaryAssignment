622175e4777323c92e8c653ee326c51df13e86a3
TheSteeNos <davidsteens2001@gmail.com>
2025-02-22 17:39:02 +0100

    Created Inch to MM converter

---
+++ tasks/taskAFunctions/inchToMmConverter.mjs
@@ -0,0 +1,6 @@
+import test from "../../tools/test.mjs";
+const tests = test("Conversion from Inch to Millimeter test");
+
+function inchToMm(inch) {
+    return (inch*25.4)
+}
--- tasks/taskAFunctions/squareRoot.mjs
+++ tasks/taskAFunctions/squareRoot.mjs
@@ -1,5 +1,5 @@
 import test from "../../tools/test.mjs";
-const tests = test("Multiplication test");
+const tests = test("Square Root Test");
 
 
 //Calculating square root using the Babylonian formula
@@ -25,8 +25,8 @@ function squareRoot(num){
 tests.isEqual(squareRoot(16), 4, 'Expects square root of 16 which is 4');
 tests.isEqual(squareRoot(0), 0, 'Expects 0 if input is 0');
 tests.isEqual(squareRoot(-16), NaN, 'Expects NaN, as there is no square root of a negative number');
-tests.isEqual(squareRoot('16'), 4, 'Expects to modify strings to integers, then multiply, expects 540');
+tests.isEqual(squareRoot('16'), 4, 'Expects to modify strings to integers, expects 4');
 tests.isNotANumber(squareRoot('hello world'), 'Expects NaN if input is not a number');
-tests.isNotANumber(squareRoot(NaN, 3, 5, 9), 'Expects NaN if input is NaN');
+tests.isNotANumber(squareRoot(NaN), 'Expects NaN if input is NaN');
 tests.isEqual(squareRoot(Infinity), Infinity, 'Expects Infinity');
 tests.isEqual(squareRoot(-Infinity), NaN, 'Expects NaN, as there is no square root of a negative number');
