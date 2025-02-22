c4fcd7daed1e0ba694d26078c21649d32f855250
TheSteeNos <davidsteens2001@gmail.com>
2025-02-22 18:47:30 +0100

    Implemented tests, and changed function accordingly

--- tasks/taskAFunctions/squareOfNumber.mjs
+++ tasks/taskAFunctions/squareOfNumber.mjs
@@ -4,5 +4,15 @@ const tests = test("Square of Number Test");
 
 
 function squareFinder(num) {
+    num = +num
     return num*num
 }
+
+tests.isEqual(squareFinder(4), 16, 'Expects square of 4 which is 16');
+tests.isEqual(squareFinder(0), 0, 'Expects square of 0 which is 0');
+tests.isEqual(squareFinder(4), 16, 'Expects square of -4 which is 16');
+tests.isEqual(squareFinder('4'), 16, 'Expects to modify strings to integers, expects square of 4 which is 16');
+tests.isNotANumber(squareFinder('hello world'), 'Expects NaN if input is not a number');
+tests.isNotANumber(squareFinder(NaN), 'Expects NaN if input is not a number');
+tests.isEqual(squareFinder(Infinity), Infinity, 'Expects Infinity');
+tests.isEqual(squareFinder(-Infinity), Infinity, 'Expects Infinity');
